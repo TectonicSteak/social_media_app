1fa58800b98eb78e433846f27ab562dcde1c5fb6
smahesh77 <smahesh7736@gmail.com>
2023-08-01 23:13:38 +0530

    fixed log in error

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,34 +1,64 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:insta_clone/mainPage.dart';
+import 'package:insta_clone/mainPage.dart'; // Import your MainPage widget
+import 'package:insta_clone/pages/homePage.dart'; // Import your HomePage widget
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
 
-  if(kIsWeb){
+  if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: 'AIzaSyAKrv5YIWT5q3wYEDpHRrXmB09b0ELN3XM',
-        appId: "1:659085382294:web:8b50f215c23484f34568b7",
-        messagingSenderId: "659085382294",
-        projectId: "socail-media-app-69316",
-        storageBucket: "socail-media-app-69316.appspot.com",)
+        apiKey: 'YOUR_API_KEY',
+        appId: 'YOUR_APP_ID',
+        messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+        projectId: 'YOUR_PROJECT_ID',
+        storageBucket: 'YOUR_STORAGE_BUCKET',
+      ),
     );
-  }else{
+  } else {
     await Firebase.initializeApp();
   }
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? token;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserInfoFromToken(); // Call the method to retrieve user info from the token
+  }
+
+  Future<void> _getUserInfoFromToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    token = prefs.getString('token'); // Get the token from shared preferences
+    print(token);
+    if (token != null) {
+      // Use the user information as needed (e.g., store it in state variables)
+    } else {
+      // Handle the case where the token is null (e.g., user is not authenticated)
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: (token != null) ? MainPage():HomePage() ,
     );
   }
 }
